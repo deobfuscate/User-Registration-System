3b285692b10a0dfcbb6642f92fedbb687ce28b5c
Dan <33081566+deobfuscate@users.noreply.github.com>
2020-09-22 20:58:03 -0700

    Error checking

--- login.php
+++ login.php
@@ -8,11 +8,14 @@
 	
 		$sql = "SELECT * FROM `users` WHERE `username` LIKE '$u' LIMIT 1";
 		$r = mysqli_query($dbc, $sql);
-		if (mysqli_affected_rows($dbc) == 0) {
+		if (!$r) {
+			$msg = "Error: " . mysqli_error($dbc);
+		}
+		elseif (mysqli_affected_rows($dbc) == 0) {
 			$msg = "Username does not exist.";
 		}
 		else {
-			while ($row = mysqli_fetch_array($r, MYSQL_ASSOC)) {
+			while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
 				if (strtolower($u) == strtolower($row['username']) && md5($p) == $row['password']) {
 					$token = md5(strtolower($row['username']).":".$row['password']);
 					setcookie("username", $u, time() + (86400 * 30), "/");
@@ -38,9 +41,9 @@
 	<h1>Login</h1>
 	<?php
 		if(isset($msg)) { 
-				print "<p class=\"error\">".$msg."</p>\n";
+			print "<p class=\"error\">".$msg."</p>\n";
 		}
-  ?>
+	?>
   <form action="<?php print $_SERVER['PHP_SELF']; ?>" method="post" enctype="multipart/form-data" name="login">
     <p><input name="username" type="text" size="32" maxlength="32" id="user" placeholder="Username" autofocus></p>
     <p><input name="password" type="password" size="32" maxlength="32" placeholder="Password"></p>
